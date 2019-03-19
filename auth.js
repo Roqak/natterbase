0d5fdbd84a989497b3358bf13ce0bdc8f95745ee
Roqak <roqak1@gmail.com>
2019-03-19 13:05:59 +0100

    routes

--- auth.js
+++ auth.js
@@ -3,26 +3,23 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'secretIsaSecretee';
-const User = {
-    username: "admin",
-    password: "admin"
-};
+var users = [
+    {
+      id: 1,
+      username: 'admin',
+      password: 'admin'
+    }
+  ];
 module.exports = passport =>{
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-        if(User.username === "admin"){
-            ({id: jwt_payload.sub}, function(err, user) {
-                if (err) {
-                    return done(err, false);
-                }
-                if (user) {
-                    return done(null, user);
-                    res.status(200).json({jso:"Done"})
-                } else {
-                    return done(null, false);
-                    // or you could create a new account
-                }
-            });
-        }
-        
-    }));
+        var user = ("admin", {id: jwt_payload.id});
+            if (user) {
+                return done(null, user);
+                res.status(200).json({jso:"Done"})
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        })
+    );
 };
